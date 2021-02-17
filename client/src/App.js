c251091d9abee492b8f53a489af86245360eed91
Keizo Hamaguchi <guchi@pop-os.localdomain>
2021-02-17 12:17:03 +0900

    imported react-router and implemented empty pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,29 @@
-import './App.css';
-import { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import {loadStripe} from '@stripe/stripe-js';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Settings from "pages/Settings";
+import Subscribe from "pages/Subscribe";
+import Snippet from "pages/Snippet";
 
 function App() {
-  const [basicPrice, setBasicPrice] = useState('');
-  const [premPrice, setPremPrice] = useState('');
-  const [pubKey, setPubKey] = useState('');
-  const stripeRef = useRef();
-
-  // this runs once on load and every time the value in the dependency array changes
-  useEffect(() => {
-    axios.get('/setup').then((res) => {
-      setBasicPrice(res.data.basicPrice);
-      setPremPrice(res.data.premiumPrice);
-      setPubKey(res.data.publishKey);
-      loadStripe(res.data.publishKey).then((stripe) => {
-        stripeRef.current = stripe;
-      });   
-    });
-  }, []);
-
-  const createCheckoutSession = async (priceID) => {
-    const response = await axios.post('/create-checkout-session', { priceID });
-    const sessionId = response.data.sessionId;
-    await stripeRef.current.redirectToCheckout({ sessionId });
-  };
-  
   return (
-    <div className="App">
-      <h1>Stripe API</h1>
-      <button onClick={() => createCheckoutSession(basicPrice)}>Basic</button>
-      <button onClick={() => createCheckoutSession(premPrice)}>Premium</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/snippet">
+          <Snippet />
+        </Route>
+        <Route path="/subscribe">
+          <Subscribe />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
