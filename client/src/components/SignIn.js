1faa83d63536d085ff206b40454ff248bbbcfccf
Keizo Hamaguchi <guchi@pop-os.localdomain>
2021-02-18 09:46:15 +0900

    should change from login to signup component

---
+++ client/src/components/SignIn.js
@@ -0,0 +1,116 @@
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { useState } from "react";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn(props) {
+  const [username, setUsername] = useState("");
+  const [pass, setPass] = useState("");
+
+  const classes = useStyles();
+
+  const createAccount = async (event) => {
+    event.preventDefault();
+    axios
+      .post("/auth/login", {
+        username: username,
+        password: pass,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+        }
+      });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="username"
+            name="username"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            onChange={(event) => {
+              setPass(event.target.value);
+            }}
+            id="password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={createAccount}
+          >
+            Sign In
+          </Button>
+          <Grid container justify="center">
+            <Grid item>
+              <Link
+                onClick={() => {
+                  props.setShowLogIn(!props.showLogIn);
+                }}
+                variant="body2"
+              >
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
